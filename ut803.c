72f2c35078b982ab9793218f57d28dd349f882f1
Martin Schuhmacher <broetchen25@gmx.net>
2009-08-10 19:15:23 +0200

    nothing yet

--- ut803.c
+++ ut803.c
@@ -2,25 +2,51 @@
 #include <stdio.h>
 #include <string.h>
 
-int serial_read(char* serchars)
+void serial_read(char* serchars)
 {
-	return VOLTS;
+	strncpy(serchars,"01009;80:",9);
+	return;
 }
 float read_value(serialreadchars,span)
 {
 	return 0.0;	
 }
-void serial_read(char* serchars)
+int read_span(char* serchars)
 {
 	return;
 }
 
 void get_unit(char* serchars, int span, char* unit)
 {
+	switch (serchars[5])
+	{
+		case ';' :
+			strcpy(unit,"V");
+			break;
+		case '9' :
+		      	strcpy(unit,"A");
+			break;
+		default:
+			break;
 	return;
-}
-int main (int argc, char *argv[])
 
+	}
+}
+/*
+ *
+1.009 V DC        01009;80:<\r><\n>
+-1.008 V DC       01008;<0:<\r><\n>
+-1.008 V DC + DC  01008;<0:<\r><\n>
+0.010 V AC        00010;806<\r><\n>
+13.4 Ohm          001343802<\r><\n>
+0.034 nF          000346802<\r><\n>
+20 °C             000204800<\r><\n>
+0.06 A DC         000069808<\r><\n>   (ca. alle 600 ms)
+64.7 mA DC        10647?80:<\r><\n>
+*
+*/
+
+int main (int argc, char *argv[])
 {
 	char serialreadchars[9+1];
 	int span;
@@ -28,10 +54,10 @@ int main (int argc, char *argv[])
 	char unit[2+1];
 	
 	serialreadchars[9]= '\0';
-  serial_read(serialreadchars);
-  span=read_span(serialreadchars);
-  value=read_value(serialreadchars,span);
-  get_unit(serialreadchars,span,unit);
+	serial_read(serialreadchars);
+	span=read_span(serialreadchars);
+	value=read_value(serialreadchars,span);
+	get_unit(serialreadchars,span,unit);
 
 // display the stuff
 	printf ("Messung: %f%s\n",value,unit);
