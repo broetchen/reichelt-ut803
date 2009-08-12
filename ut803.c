d1d57d68f4dccace4aedd419a70fe940da8cefa7
Martin Schuhmacher <broetchen25@gmx.net>
2009-08-12 22:26:23 +0200

    foo

--- ut803.c
+++ ut803.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <termios.h> /* POSIX terminal control definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
-void serial_read(char* serchars)
+void serial_read(char* serchars, int devfd)
 {
+	int nbytes=0;
+	char bufptr[128];
+	nbytes += read(devfd,bufptr, 1);
 	strncpy(serchars,"01009;80:",9);
 	return;
 }
@@ -45,16 +59,41 @@ void get_unit(char* serchars, int span, char* unit)
 64.7 mA DC        10647?80:<\r><\n>
 *
 */
+int serial_init (char* dev)
+{
+	struct termios options;
+	int fd; /* File descriptor for the port */
+	fd = open(dev, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (fd == -1)
+		perror("open_port: Unable to open port ");
+	else
+		fcntl(fd, F_SETFL, 0);
 
+	tcgetattr(fd, &options);
+	cfsetispeed(&options, B1200);
+	cfsetospeed(&options, B1200);
+
+	options.c_cflag |= (CLOCAL | CREAD);
+	options.c_cflag &= ~PARENB;
+	options.c_cflag &= ~CSTOPB;
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS8;
+	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+	tcsetattr(fd, TCSANOW, &options);
+	return (fd);		
+}
 int main (int argc, char *argv[])
 {
 	char serialreadchars[9+1];
 	int span;
 	float value;
 	char unit[2+1];
-	
+	char serdev[] = "/dev/ttyS0";
+	int devfd;
+
+	devfd=serial_init(serdev);
 	serialreadchars[9]= '\0';
-	serial_read(serialreadchars);
+	serial_read(serialreadchars,devfd);
 	span=read_span(serialreadchars);
 	value=read_value(serialreadchars,span);
 	get_unit(serialreadchars,span,unit);
@@ -62,6 +101,7 @@ int main (int argc, char *argv[])
 // display the stuff
 	printf ("Messung: %f%s\n",value,unit);
 //		01009;80:
+	close(devfd);
 	return 0;
 
 }
